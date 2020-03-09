17d96471bc6551ff69443eed1ff500a14c0dceb4
Marco Aurélio Graciotto Silva <magsilva@utfpr.edu.br>
2020-03-09 18:42:06 -0300

    Make code happier for Java 9 or greater

---
+++ src/module-info.java
@@ -0,0 +1,19 @@
+module simse {
+	exports simse.codegenerator;
+	exports simse.codegenerator.enginegenerator;
+	exports simse.codegenerator.logicgenerator.dialoggenerator;
+	exports simse.modelbuilder.mapeditor;
+	exports simse.modelbuilder.rulebuilder;
+	exports simse.codegenerator.guigenerator;
+	exports simse.codegenerator.utilgenerator;
+	exports simse.codegenerator.explanatorytoolgenerator;
+	exports simse.modelbuilder.startstatebuilder;
+	exports simse.modelbuilder.actionbuilder;
+	exports simse.codegenerator.logicgenerator;
+	exports simse.modelbuilder.objectbuilder;
+	exports simse.modelbuilder.graphicsbuilder;
+	exports simse.modelbuilder;
+	exports simse.codegenerator.stategenerator;
+
+	requires java.desktop;
+}
